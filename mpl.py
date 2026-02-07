3d24a160ed2e52f73029d08966e2a80011c683d0
Himeshkar11 <himeshkarm0111@gmail.com>
2026-02-08 05:05:27 +0530

    completed day 0 intro to matplotlib

---
+++ mpl.py
@@ -0,0 +1,8 @@
+# import matplotlib
+# print(matplotlib.__version__) # 3.10.7
+import matplotlib.pyplot as plt 
+# creating basic plot using python lists 
+x = [2023,2024,2025,2026]
+y = [38,40,57,68]
+plt.plot(x,y)
+plt.show()
