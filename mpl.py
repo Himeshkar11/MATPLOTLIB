6302ce00f7648acba758bcf56a69f595b1545227
Himeshkar11 <himeshkarm0111@gmail.com>
2026-02-09 03:56:06 +0530

    matplotlib day2

--- mpl.py
+++ mpl.py
@@ -1,8 +1,25 @@
 # import matplotlib
 # print(matplotlib.__version__) # 3.10.7
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import numpy as np 
 # creating basic plot using python lists 
+# x = [2023,2024,2025,2026]
+# y = [38,40,57,68]
+# plt.plot(x,y)
+# plt.show()
+# creating basic plot using numpy arrays becuse they are faster 
+# a = np.array([2023,2024,2025,2026])
+# b = np.array([38,40,57,68])
+# plt.plot(a,b)
+# plt.show()
+# markers in matplot
+# x = [2023,2024,2025,2026]
+# y = [38,40,57,68]
+# plt.plot(x,y,marker="." )
+# plt.show()
+# changing the color of the marker 
 x = [2023,2024,2025,2026]
 y = [38,40,57,68]
-plt.plot(x,y)
+plt.plot(x,y,marker=".",markersize=20,markerfacecolor="cyan")
 plt.show()
+
